6ab18d75ed4705b05e56ab0d50a0ac5b46781255
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-07-12 22:08:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function palindrome(str){
 	let nstr=str.toLowerCase();
 	let noSpaces = nstr.replace(/ /g, "");
 
-	for(let i=0;i< Math.floor(noSpaces.length / 2);;i++){
+	for(let i=0;i< Math.floor(noSpaces.length / 2);i++){
 		if(noSpaces[i]!==noSpaces[noSpaces.length-1-i]){
 			return false;
 
